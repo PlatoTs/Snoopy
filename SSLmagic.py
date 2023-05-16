1596c313522107da5728d0be1c73a97796748f54
PlatoTs <132805742+PlatoTs@users.noreply.github.com>
2023-05-16 20:04:22 +0930

    Add files via upload

---
+++ SSLmagic.py
@@ -0,0 +1,35 @@
+import pyfiglet
+import colorama
+import sys
+import ssl
+import socket
+import os 
+from urllib.parse import urlparse
+from colorama import Fore
+colorama.init()
+os.system('cls' if os.name == 'nt' else 'clear')
+#Art Banner 
+art = pyfiglet.figlet_format("WhereSSL")
+print(Fore.CYAN + art)
+print("-" * 50)
+#target
+hostname = input("Enter IP or URL: ")
+#ssl connection 
+context = ssl.create_default_context()
+#default port 443 HTTPS and establish encrypted SSL connection
+with socket.create_connection((hostname, 443)) as sock:
+    with context.wrap_socket(sock, server_hostname=hostname) as ssock:
+        cert = ssock.getpeercert() # Where certificate is stored // f-strings makes it easier and readable
+        if cert and ('subjectAltName' in cert or 'subject' in cert):
+            print(Fore.CYAN + f"The certificate is valid for {cert['subjectAltName']}") 
+            print(Fore.CYAN + f"Valid from: {cert['notBefore']}")
+            print(Fore.CYAN + f"Valid until: {cert['notAfter']}")
+            url = urlparse(f"https://{hostname}")
+            if url.scheme == "https":
+                print(Fore.GREEN + "The website is using HTTPS")
+            else:
+                print(Fore.RED + "The website is not using HTTPS")
+            print(f"Issued by: {cert['issuer']}")
+            print(f"Serial number: {cert['serialNumber']}")
+        else:
+            print("The website's certificate is invalid or not found") 
---
+++ Snoopy.py
@@ -0,0 +1,33 @@
+import os
+import colorama
+from colorama import *
+import pyfiglet
+
+colorama.init()
+
+#clears the terminal 
+os.system('cls' if os.name == 'nt' else 'clear')
+
+print("Welcome to Snooping".center(80, "-"))
+
+#Attack Options
+print("List of Recon:")
+print("-" * 80)
+print("[1].Ports scanner ")
+print("[2].SSL Finder ")
+print("[3].Crawler ")
+print("[4].Headers ")
+
+
+#Imports scripts for each Recon.
+selection = input("Input number: ")
+if selection == "1":
+	import Ports_scanning
+elif selection == "2":
+	import SSLmagic
+elif selection == "3":
+	import crawk
+elif selection == "4":
+	import HyperHeaders
+else:
+	print("Invalid Selection. Please enter from 1-4.")
